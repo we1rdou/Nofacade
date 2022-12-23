aea22fada6f9111dc7508a9c2b1cb2dc1b9e08c6
Leandro Flores <109381840+we1rdou@users.noreply.github.com>
2022-12-23 14:38:51 -0500

    Add files via upload

---
+++ pfacade/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ComprobarLiquidos liquidos = new ComprobarLiquidos();
+        liquidos.comprobar();
+        
+        ComprobarAsiento asiento = new ComprobarAsiento();
+        asiento.comprobar();
+
+        ComprobarEspejos espejos = new ComprobarEspejos();
+        espejos.comprobar();
+
+        Arrancar arrancar = new Arrancar();
+        arrancar.encenderContacto();
+
+        System.Console.WriteLine("--------------------------------------------------");
+        System.Console.WriteLine("Mantenimiento preventivo correcto");
+        System.Console.WriteLine("--------------------------------------------------");
+    }
+}
